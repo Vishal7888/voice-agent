e3e42ca2faacd9e2de97f4c59042334151d9332c
Vishal7888 <62740147+Vishal7888@users.noreply.github.com>
2025-07-27 00:12:22 +0530

    tts.js

--- tts.js
+++ tts.js
@@ -1,20 +1,30 @@
 // tts.js
 import textToSpeech from "@google-cloud/text-to-speech";
+import util from "util";
 import dotenv from "dotenv";
 dotenv.config();
 
+const credentials = JSON.parse(process.env.GOOGLE_TTS_KEY);
+
 const client = new textToSpeech.TextToSpeechClient({
-  credentials: JSON.parse(process.env.GOOGLE_TTS_KEY)
+  credentials,
 });
 
 export const GoogleTTS = {
   async synthesize(text) {
-    const [response] = await client.synthesizeSpeech({
+    const request = {
       input: { text },
-      voice: { languageCode: "en-US", ssmlGender: "NEUTRAL" },
-      audioConfig: { audioEncoding: "LINEAR16" }
-    });
+      voice: {
+        languageCode: "en-US",
+        ssmlGender: "NEUTRAL",
+      },
+      audioConfig: {
+        audioEncoding: "LINEAR16",
+        sampleRateHertz: 8000,
+      },
+    };
 
+    const [response] = await client.synthesizeSpeech(request);
     return response.audioContent;
-  }
+  },
 };
